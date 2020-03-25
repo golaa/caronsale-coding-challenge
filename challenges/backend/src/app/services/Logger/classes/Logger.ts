9e4a91e8016a16bad4044b0ff8805ec47640f192
Jan Schürmann <jan.schuermann.ph@gmail.com>
2020-03-25 10:52:30 +0100

    Adjust Logger to display timestamps

--- challenges/backend/src/app/services/Logger/classes/Logger.ts
+++ challenges/backend/src/app/services/Logger/classes/Logger.ts
@@ -2,15 +2,21 @@ import {ILogger} from "../interface/ILogger";
 import {injectable} from "inversify";
 import "reflect-metadata";
 
+// tslint:disable:no-console
+
 @injectable()
 export class Logger implements ILogger {
 
     public constructor() {
+        console.clear();
     }
 
+    private static getCurrentTimestamp(): string {
+        return new Date().toISOString();
+    }
 
     public log(message: string): void {
-        console.log(`[LOG]: ${message}`);
+        console.log(`[LOG ${Logger.getCurrentTimestamp()}]: ${message}`);
     }
 
 }
